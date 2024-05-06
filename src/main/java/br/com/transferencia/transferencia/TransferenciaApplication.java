7b00c73d1da3955593ca08deebbaef2aa906bd2a
Allan Rodrigues <allan.asr@hotmail.com>
2024-05-06 11:14:46 -0300

    init

---
+++ src/main/java/br/com/transferencia/transferencia/TransferenciaApplication.java
@@ -0,0 +1,13 @@
+package br.com.transferencia.transferencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferenciaApplication.class, args);
+	}
+
+}
